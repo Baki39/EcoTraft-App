cbb49a0c6f3366f5a7a20aa13b9bd00f2413b56e
Bahrudin <bakivfxdautovicicloud.com@Bahrudins-iMac.local>
2026-04-06 02:34:04 +0100

    Add Android platform and capacitor config

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ecotraft.app',
+  appName: 'EcoTraft',
+  webDir: 'dist'
+};
+
+export default config;
